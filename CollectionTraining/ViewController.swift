4c7dd5576f5734d0482787c664457a18bffcfc4d
Julien <juliengenoud@gmail.com>
2015-12-04 08:59:04 +0800

    push images

--- CollectionTraining/ViewController.swift
+++ CollectionTraining/ViewController.swift
@@ -13,12 +13,20 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     var DataTable: [String] = ["xxx1", "xxx2", "xxx3","xxx4","xxx5","xxx6","xxx7","xxx8","xxx9","xxx10","xxx11","xxx12","xxx13","xxx14","xxx15","xxx16","xxx17","xxx18","xxx19","xxx20"]
     var ImageTable: [String] = ["Ingredient1.png","Ingredient2.jpg","Ingredient3.JPG","Ingredient4.jpg","Ingredient5.jpg","Ingredient6.jpg","Ingredient7.jpg","Ingredient8.jpg","Ingredient9.jpg","Ingredient10.jpg","Ingredient11.jpg","Ingredient12.jpg","Ingredient13.jpg","Ingredient14.jpg","Ingredient15.jpg","Ingredient16.jpg","Ingredient17.jpg","Ingredient18.jpg","Ingredient19.jpg","Ingredient20.jpg"]
     
-    
+    @IBOutlet weak var FridgeButton: UITabBarItem!
     @IBOutlet weak var NavigationTabBar: UITabBar!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let itemIndex = 0
+        let bgColor = UIColor(red: 0.08, green: 0.726, blue: 0.702, alpha: 1.0)
+        
+        let itemWidth = NavigationTabBar.frame.width / CGFloat(NavigationTabBar.items!.count)
+        let mutiply = itemWidth * CGFloat(itemIndex);
+        let bgView = UIView(frame: CGRectMake(mutiply, 0, itemWidth, NavigationTabBar.frame.height))
+        bgView.backgroundColor = bgColor
+        NavigationTabBar.insertSubview(bgView, atIndex: 0)
     }
 
     override func didReceiveMemoryWarning() {
